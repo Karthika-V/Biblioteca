90cabfd062bdefd7495d476f6546051b3bb0590f
karthika <karthika.vsdvn@gmail.com>
2015-09-15 20:59:40 +0530

    Created main class biblioteca Application and edited build.xml

---
+++ src/com/tw/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.tw.biblioteca;
+
+public class BibliotecaApplication {
+
+    public static void main(String[] args) {
+        BibliotecaApplication bibliotecaApplication = new BibliotecaApplication();
+        bibliotecaApplication.start();
+    }
+
+    public void start() {
+        System.out.println("Welcome to Biblioteca");
+    }
+}
