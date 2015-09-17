9486eda2bd2968c9a1ec4a459f0cc0ad3be32cb3
karthika <karthika.vsdvn@gmail.com>
2015-09-18 00:04:46 +0530

    Created session for keeping track of user

---
+++ src/com/tw/biblioteca/Session.java
@@ -0,0 +1,14 @@
+package com.tw.biblioteca;
+
+public class Session {
+
+    private User user;
+
+    public Session(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
