ec326df2c4a450f5c5fa5c12291a0371e9bc4452
karthika <karthika.vsdvn@gmail.com>
2015-09-18 11:08:10 +0530

    Added back files which was removed by mistake

---
+++ src/com/tw/biblioteca/Authenticator.java
@@ -0,0 +1,29 @@
+//Authenticator authenticates user credentials
+package com.tw.biblioteca;
+
+import java.util.ArrayList;
+
+public class Authenticator {
+
+    private ArrayList<User> userCredentials = new ArrayList<>();
+
+    public ArrayList userData() {
+        userCredentials.add(new User("111-1111", "qwerty", "user1", "Def@xyz.com", "100", User.Role.USER));
+        userCredentials.add(new User("123-4567", "abcd", "user2", "Def@xyz.com", "100", User.Role.LIBRARIAN));
+        return userCredentials;
+    }
+
+    public User checkCredentials(String inputLibraryNumber, String password) {
+        User currentUser = new User(inputLibraryNumber, password, "", "", "", User.Role.GUEST);
+        if (userCredentials.contains(currentUser)) {
+            for (User user : userCredentials) {
+                if (user.equals(currentUser)) {
+                    if (user.checkPassword(currentUser)) {
+                        return user;
+                    }
+                }
+            }
+        }
+        return currentUser;
+    }
+}
---
+++ test/com/tw/biblioteca/UserTest.java
@@ -0,0 +1,71 @@
+package com.tw.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void shouldHaveUserDetails() {
+        User user = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+
+        assertEquals(true, !(user.toString().isEmpty()));
+    }
+
+    @Test
+    public void shouldNotBeNull() {
+        User user = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+
+        assertNotEquals(user, null);
+    }
+
+    @Test
+    public void shouldBeEqualToItself() {
+        User user = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+
+        assertEquals(user, user);
+    }
+
+    @Test
+    public void shouldBeEqualToOtherInstanceOfSameClass() {
+        User user1 = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+        User user2 = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+        assertEquals(user1, user2);
+    }
+
+    @Test
+    public void shouldNotBeEqualToAnotherType() {
+        User user = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+
+        assertNotEquals(user, "some string");
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeForSameUserName() {
+        User user1 = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+        User user2 = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+
+        assertEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    public void shouldHaveDifferentHashCodeForDifferentUserName() {
+        User user1 = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+        User user2 = new User("111-1111", "abcd", "Def", "Def@xyz.com", "100", User.Role.USER);
+
+        assertNotEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    public void shouldDisplayUserDetails() {
+        User user = new User("111-1111", "abcd", "Abc", "Def@xyz.com", "100", User.Role.USER);
+
+        assertEquals("Library Number:111-1111\n" +
+                " User Name:Abc\n" +
+                " User Mail:Def@xyz.com\n" +
+                " Phone Number:100\n" +
+                " Role:USER\n", user.displayUserDetails());
+    }
+
+}
