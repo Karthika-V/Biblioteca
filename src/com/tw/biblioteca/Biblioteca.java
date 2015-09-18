2e4e578851b13305207f0a5b4c5936818d0f5929
karthika <karthika.vsdvn@gmail.com>
2015-09-18 08:17:20 +0530

    Added Session to Biblioteca

--- src/com/tw/biblioteca/Biblioteca.java
+++ src/com/tw/biblioteca/Biblioteca.java
@@ -6,11 +6,13 @@ public class Biblioteca {
     private ConsoleView consoleView;
     private MenuOptions menuOptions;
     private User user;
+    private Session session;
 
-    public Biblioteca(ConsoleView consoleView, MenuOptions menuOptions, User user) {
+    public Biblioteca(ConsoleView consoleView, MenuOptions menuOptions, User user, Session session) {
         this.consoleView = consoleView;
         this.menuOptions = menuOptions;
         this.user = user;
+        this.session = session;
     }
 
     public void start() {
@@ -21,6 +23,7 @@ public class Biblioteca {
     }
 
     private void run() {
+        User user = session.getUser();
         consoleView.menu();
         menuOptions.optionHandler(consoleView.inputFromUser());
     }
